732cd7db219c37baed4e0d5b51775feb663512e0
minh quang <minhquangkid@gmail.com>
2022-07-11 22:28:11 +0700

    create border done !

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
+import { STAFFS } from "./shared/staffs.jsx"; // dùng {} khi nó là 1 giá trị không phải là component
 import StaffList from "../src/Components/StaffListComponent"; // không dùng  {} vì StaffList là 1 component
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <StaffList />
+        <Navbar dark color="primary">
+          <div className="fluid">
+            <NavbarBrand href="/">Ứng dụng quản lý nhân sự</NavbarBrand>
+          </div>
+        </Navbar>
+        <div className="container">
+          <StaffList nhanvien={STAFFS} />
+        </div>
       </div>
     );
   }
--- src/Components/StaffListComponent.js
+++ src/Components/StaffListComponent.js
@@ -1,10 +1,34 @@
 "use strict";
 import React, { Component } from "react"; // phải có dòng này mới dùng các component được
-import { STAFFS } from "../shared/staffs.jsx"; // dùng {} khi nó là 1 giá trị không phải là component
 
 //console.log(STAFFS);
-function StaffList() {
-  return <p>{STAFFS[0].name}</p>;
+function StaffList(props) {
+  const mystyle = {
+    boxSizing: "border-box",
+    boderSize: "0.5px",
+    borderStyle: "solid",
+    margin: "5px 1px",
+    padding: "5px",
+  };
+  const list = props.nhanvien.map((e) => {
+    return (
+      <div className="col-12 col-md-6 col-lg-4" style={mystyle}>
+        {e.name}
+      </div>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <div className="row">
+        {/* <p>{STAFFS[0].name}</p> */}
+        {list}
+      </div>
+      <div></div>
+    </React.Fragment>
+  );
 }
 
 export default StaffList;
+// phải tạo trong thư mục là thư mục con của src thì React mới hoạt động được
+// tất cả dữ liệu API bên ngoài mà muốn đưa vào sử dụng như staffs.jsx đều phải nhét trong thư mục con của src thì React mới hoạt động được (ví dụ như thư mục shared ở đây)
